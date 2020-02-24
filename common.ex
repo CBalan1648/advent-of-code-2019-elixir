e5d9570a8033c835acb039e856b658334adfcf3b
Catalin Balan <catalin.balan@iquestgroup.com>
2020-02-24 09:54:02 +0200

    Day 3 - Part 1 WIP

--- common.ex
+++ common.ex
@@ -44,4 +44,36 @@ defmodule Common do
     list
   end
 
+  # Day 3 part 1
+  def mapToPoint(value, acc) do
+
+    direction = List.first(value)
+    distance = List.first(Tuple.to_list(Integer.parse(List.last(value))))
+
+    case direction do
+      "R" ->
+        destinationPoint = [List.first(acc), List.last(acc) + distance]
+        {destinationPoint, destinationPoint}
+
+      "L" ->
+        destinationPoint = [List.first(acc), List.last(acc) - distance]
+        {destinationPoint, destinationPoint}
+
+      "D" ->
+        destinationPoint = [List.first(acc) - distance, List.last(acc)]
+        {destinationPoint, destinationPoint}
+
+      "U" ->
+        destinationPoint = [List.first(acc) + distance, List.last(acc)]
+        {destinationPoint, destinationPoint}
+    end
+  end
+
+  def mapToLine(value, acc) do
+    
+    IO.inspect value
+    IO.inspect acc
+    
+  end
+
 end
--- day-3.exs
+++ day-3.exs
@@ -1,11 +1,22 @@
-inputLineOne = ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"]
+# inputLineOne = ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"]
 # inputLineTwo = ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"]
 
-#inputLineOne = ["R8", "U5", "L5", "D3"]
-#inputLineTwo = ["U7", "R6", "D4", "L4"]
+inputLineOne = ["R8", "U5", "L5", "D3"]
+inputLineTwo = ["U7", "R6", "D4", "L4"]
 
-#String.split(&1, '', parts: 2)
 
-splittedData = inputLineOne |> Enum.map(&(String.split(&1, "", trim: true, parts: 2)))
 
-IO.inspect splittedData
+splittedData =
+  [inputLineOne, inputLineTwo]
+  |> Enum.map(
+    fn input ->
+        input|> Enum.map(&String.split(&1, "", trim: true, parts: 2))
+      |> Enum.map_reduce([0, 0], &Common.mapToPoint(&1, &2))
+      |> Tuple.to_list
+      |> List.first
+      |> Enum.map_reduce(0, &Common.mapToLine(&1, &2))
+    end
+  )
+  
+
+IO.inspect(splittedData)
